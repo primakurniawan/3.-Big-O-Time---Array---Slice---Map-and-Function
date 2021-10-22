fc81d4eb7bcc3140be761525b0abfdb03b7a3997
Prima Tondy Arya Kurniawan <primakurniawan666@gmail.com>
2021-10-22 10:15:38 +0700

    4

---
+++ 4.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func munculSekali(angka string) []int {
+
+	// your code here
+	list := []int{}
+	intSlice := strings.Split(angka, "")
+	for _, item := range intSlice {
+		i, _ := strconv.Atoi(item)
+		if _, value := allKeys[i]; !value {
+			allKeys[i] = true
+			list = append(list, i)
+		}
+
+	}
+	return list
+
+}
+
+func main() {
+
+	fmt.Println(munculSekali("1234123")) // [4]
+
+	fmt.Println(munculSekali("76523752")) // [6, 3]
+
+	fmt.Println(munculSekali("12345")) // [1 2 3 4 5]
+
+	fmt.Println(munculSekali("1122334455")) // []
+
+	fmt.Println(munculSekali("0872504")) // [8 7 2 5 4]
+
+}
